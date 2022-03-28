8edb081882e28e977900f20e0b976486127bb7a8
itangishaka1 <abdullahitangishaka@gmail.com>
2022-03-27 23:07:23 -0400

    make use of .env file

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import MovieCard from "./MovieCard"
 import "./App.css"
 import SearchIcon from "./search.svg"
 
+const { REACT_APP_API_SECRET_KEY } = process.env
 // Register a free here to get the api key to use https://www.omdbapi.com/apikey.aspx
-const API_URL = "http://www.omdbapi.com?apikey=71538a07"
+const API_URL = `http://www.omdbapi.com?apikey=${REACT_APP_API_SECRET_KEY}`
 /*We want to fetch the data as soon as the page load, we use the useEffect hook*/
 
-const movie1 = {
-  Title: "Spiderman",
-  Year: "2010",
-  imdbID: "tt1785572",
-  Type: "movie",
-  Poster: "N/A",
-}
+// const movie1 = {
+//   Title: "Spiderman",
+//   Year: "2010",
+//   imdbID: "tt1785572",
+//   Type: "movie",
+//   Poster: "N/A",
+// }
 
 const App = () => {
   const [movies, setMovies] = useState([])
